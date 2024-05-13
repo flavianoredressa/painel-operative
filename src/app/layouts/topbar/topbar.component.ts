03d2ae680b66ef48440f8b4161a349ddd26dd5af
Gabriel Lima <99371050+gabriellima3@users.noreply.github.com>
2024-05-13 16:19:59 -0300

    style(topbar): layout adjustments

--- src/app/layouts/topbar/topbar.component.ts
+++ src/app/layouts/topbar/topbar.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ImgFallbackDirective, ShortNamePipe } from '@burand/angular';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { LucideAngularModule } from 'lucide-angular';
 
+import { NgClass } from '@angular/common';
 import { SessionContext } from '@contexts/session.context';
 import { SideBarContext } from '@contexts/side-bar-context';
 import { environment } from '@environment';
@@ -13,15 +14,26 @@ import { environment } from '@environment';
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NgbDropdownModule, LucideAngularModule, ShortNamePipe, ImgFallbackDirective]
+  imports: [NgbDropdownModule, LucideAngularModule, ShortNamePipe, ImgFallbackDirective, NgClass]
 })
 export class TopbarComponent {
   private router = inject(Router);
   private useSession = inject(SessionContext);
   public useSideBar = inject(SideBarContext);
 
+  isMobile = false;
+
   version = environment.appVersion;
   currentUser = this.useSession.getLoggedUser;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isMobile = event.target.innerWidth > 768 ? false : true;
+  }
+
+  constructor() {
+    this.isMobile = document.documentElement.clientWidth < 768 ? true : false;
+  }
 
   async logout() {
     await this.useSession.logout();
