de9a39e0fe339a63d13543ff2075e071df56f834
Flaviano Redressa <flaviano@operative.com.br>
2024-06-03 19:28:54 -0300

    wip

--- src/app/core/repositories/status-sale.repository.ts
+++ src/app/core/repositories/status-sale.repository.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AddDocument } from '@burand/angular';
 import { environment } from '@environment';
 import { StatusSale } from '@models/status-sale';
-import { lastValueFrom, map } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
+
+type CreateStatusSale = Pick<AddDocument<StatusSale>, 'name' | 'active'>;
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +15,22 @@ export class StatusSaleRepository {
   constructor(private httpClient: HttpClient) {}
 
   getAll() {
-    return this.httpClient.get<StatusSale[]>(`${environment.urlApi}/status-sale`).pipe(
-      map((res: any) => {
-        return res.lista;
-      })
-    );
+    return this.httpClient.get<StatusSale[]>(`${environment.urlApi}/status-sale`);
+  }
+
+  async getUserById(id: string) {
+    const statusSale = await lastValueFrom(this.httpClient.get<StatusSale>(`${environment.urlApi}/status-sale/${id}`));
+    return statusSale;
+  }
+
+  async create(statusSale: CreateStatusSale) {
+    await lastValueFrom(this.httpClient.post(`${environment.urlApi}/status-sale`, statusSale));
+    return;
+  }
+
+  async update(id: string, statusSale: CreateStatusSale) {
+    await lastValueFrom(this.httpClient.put(`${environment.urlApi}/status-sale/${id}`, statusSale));
+    return;
   }
 
   async delete(id: string) {
--- src/app/pages/status-sale/status-sale-create/status-sale-create.component.ts
+++ src/app/pages/status-sale/status-sale-create/status-sale-create.component.ts
@@ -1,20 +1,76 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { getRouterParam } from '@utils/get-router-param';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IsLoadingDirective, getRouterParam } from '@burand/angular';
+import { InputComponent } from '@forms/input/input.component';
+import { errorTailorImports } from '@ngneat/error-tailor';
+import { StatusSaleRepository } from '@repositories/status-sale.repository';
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-satus-sale-create',
   standalone: true,
-  imports: [],
-  templateUrl: './status-sale-create.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  imports: [ReactiveFormsModule, InputComponent, errorTailorImports, IsLoadingDirective],
+  templateUrl: './status-sale-create.component.html'
 })
 export class SatusSaleCreateComponent implements OnInit {
+  private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+  private toastrService = inject(ToastrService);
+  private statusSaleRepository = inject(StatusSaleRepository);
+
   idStatusSales = getRouterParam('id');
 
-  ngOnInit() {
-    console.log(this.idStatusSales);
+  loading = signal(false);
+  submitting = signal(false);
 
-    if (this.idStatusSales) {
-      console.log('edit');
+  formGroup = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    active: [true, [Validators.required]]
+  });
+
+  async ngOnInit() {
+    try {
+      if (this.idStatusSales) {
+        this.loading.set(true);
+        const statusSale = await this.statusSaleRepository.getUserById(this.idStatusSales);
+        this.formGroup.patchValue(statusSale);
+        this.loading.set(false);
+      }
+    } catch (error) {
+      this.loading.set(false);
+      this.toastrService.error('Não foi possível carregar os dados.');
+      console.error(error);
+    }
+  }
+
+  async handleSubmit() {
+    if (this.formGroup.invalid) {
+      this.toastrService.error('Verifique os campos e tenta novamente.');
+      return;
+    }
+
+    this.submitting.set(true);
+
+    try {
+      const { name, active } = this.formGroup.value;
+      const statusSale = {
+        active,
+        name
+      };
+
+      if (!this.idStatusSales) {
+        await this.statusSaleRepository.create(statusSale);
+      } else {
+        await this.statusSaleRepository.update(this.idStatusSales, statusSale);
+      }
+      this.toastrService.success(`Status Sales ${!this.idStatusSales ? 'cadastrado' : 'atualizado'} com sucesso.`);
+      this.router.navigateByUrl('/status-sales');
+    } catch (error) {
+      this.toastrService.error('Não foi possível salvar os dados.');
+      console.error(error);
+    } finally {
+      this.submitting.set(false);
     }
   }
 }
