e51e26fc5203eb4743658b00ac5d611ba7020651
João Pedro G Furtado <82687549+JoaoPedroFurtado@users.noreply.github.com>
2024-06-04 15:39:54 -0300

    feat: deleted status-task

--- src/app/pages/status-task/status-task-list/status-task-list.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-model-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './status-task-list.component.html'
-})
-export class StatusTaskListComponent {}
