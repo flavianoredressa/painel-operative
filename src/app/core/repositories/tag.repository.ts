2fb4cb46f9bebcd776918cf81fbb4062818d1680
Fuzetto-pixel <mayconfuzetto@outlook.com>
2024-06-06 15:18:46 -0300

    feat: refac lis tag

--- src/app/core/repositories/tag.repository.ts
+++ src/app/core/repositories/tag.repository.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environment';
 import { Tag } from '@models/tag';
-import { lastValueFrom, map } from 'rxjs';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +12,7 @@ export class TagRepository {
   constructor(private httpClient: HttpClient) {}
 
   getAll() {
-    return this.httpClient.get<Tag[]>(`${environment.urlApi}/tags`).pipe(
-      map((res: any) => {
-        return res.lista;
-      })
-    );
+    return this.httpClient.get<Tag[]>(`${environment.urlApi}/tags`).pipe();
   }
 
   async delete(id: string) {
