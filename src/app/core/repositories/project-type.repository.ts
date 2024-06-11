8a95a9a4a8e5865747a774569180b8818ecc2102
João Pedro G Furtado <82687549+JoaoPedroFurtado@users.noreply.github.com>
2024-06-11 16:44:40 -0300

    feat(project-type): add status nos switches, mandando pro banco.

--- src/app/core/repositories/project-type.repository.ts
+++ src/app/core/repositories/project-type.repository.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AddDocument } from '@burand/angular';
 import { environment } from '@environment';
 import { ProjectType } from '@models/project-type';
 import { lastValueFrom } from 'rxjs';
+
+type CreateProjectType = Pick<AddDocument<ProjectType>, 'name' | 'active'>;
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +16,23 @@ export class ProjectTypeRepository {
 
   getAll() {
     return this.httpClient.get<ProjectType[]>(`${environment.urlApi}/project-type`);
+  }
+
+  async getStatusById(id: string) {
+    const projectType = await lastValueFrom(
+      this.httpClient.get<ProjectType>(`${environment.urlApi}/project-type/${id}`)
+    );
+    return projectType;
+  }
+
+  async create(projectType: CreateProjectType) {
+    await lastValueFrom(this.httpClient.post(`${environment.urlApi}/project-type`, projectType));
+    return;
+  }
+
+  async update(id: string, projectType: CreateProjectType) {
+    await lastValueFrom(this.httpClient.put(`${environment.urlApi}/project-type/${id}`, projectType));
+    return;
   }
 
   async delete(id: string) {
