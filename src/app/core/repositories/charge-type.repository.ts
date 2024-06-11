89f1df8cd8b5856d9c1717b16b9476aae524fe54
João Pedro G Furtado <82687549+JoaoPedroFurtado@users.noreply.github.com>
2024-06-11 16:43:21 -0300

    feat(charge-type): add status nos switches, mandando pro banco.

--- src/app/core/repositories/charge-type.repository.ts
+++ src/app/core/repositories/charge-type.repository.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AddDocument } from '@burand/angular';
 import { environment } from '@environment';
 import { ChargeType } from '@models/charge-type';
 import { lastValueFrom } from 'rxjs';
+
+type CreateChargeType = Pick<AddDocument<ChargeType>, 'name' | 'active'>;
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +16,21 @@ export class ChargeTypeRepository {
 
   getAll() {
     return this.httpClient.get<ChargeType[]>(`${environment.urlApi}/charge-type`);
+  }
+
+  async getStatusById(id: string) {
+    const chargeType = await lastValueFrom(this.httpClient.get<ChargeType>(`${environment.urlApi}/charge-type/${id}`));
+    return chargeType;
+  }
+
+  async create(chargeType: CreateChargeType) {
+    await lastValueFrom(this.httpClient.post(`${environment.urlApi}/charge-type`, chargeType));
+    return;
+  }
+
+  async update(id: string, chargeType: CreateChargeType) {
+    await lastValueFrom(this.httpClient.put(`${environment.urlApi}/charge-type/${id}`, chargeType));
+    return;
   }
 
   async delete(id: string) {
