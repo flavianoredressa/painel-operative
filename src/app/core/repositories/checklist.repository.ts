9fb9884723abde45b40ddedbda827f403d306d3c
Venator2345 <vitorhugormartz@gmail.com>
2024-06-12 00:49:39 -0300

    feat: add create form for checklist, tag and user types

--- src/app/core/repositories/checklist.repository.ts
+++ src/app/core/repositories/checklist.repository.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AddDocument } from '@burand/angular';
 import { environment } from '@environment';
 import { Checklist } from '@models/checklist';
 import { lastValueFrom } from 'rxjs';
+
+type CreateChecklist = Pick<AddDocument<Checklist>, 'name' | 'active'>;
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +15,22 @@ export class ChecklistRepository {
   constructor(private httpClient: HttpClient) {}
 
   getAll() {
-    return this.httpClient.get<[Checklist]>(`${environment.urlApi}/checklists`);
+    return this.httpClient.get<Checklist[]>(`${environment.urlApi}/checklists`);
+  }
+
+  async getStatusById(id: string) {
+    const checklist = await lastValueFrom(this.httpClient.get<Checklist>(`${environment.urlApi}/checklists/${id}`));
+    return checklist;
+  }
+
+  async create(checklist: CreateChecklist) {
+    await lastValueFrom(this.httpClient.post(`${environment.urlApi}/checklists`, checklist));
+    return;
+  }
+
+  async update(id: string, checklist: CreateChecklist) {
+    await lastValueFrom(this.httpClient.put(`${environment.urlApi}/checklists/${id}`, checklist));
+    return;
   }
 
   async delete(id: string) {
--- src/app/core/repositories/user-types.repository.ts
+++ src/app/core/repositories/user-types.repository.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AddDocument } from '@burand/angular';
 import { environment } from '@environment';
 import { UserTypes } from '@models/user-types';
 import { lastValueFrom } from 'rxjs';
+
+type CreateUserType = Pick<AddDocument<UserTypes>, 'name' | 'active'>;
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +18,23 @@ export class UserTypesRepository {
     return this.httpClient.get<UserTypes[]>(`${environment.urlApi}/user-types`);
   }
 
+  async getStatusById(id: string) {
+    const userType = await lastValueFrom(this.httpClient.get<UserTypes>(`${environment.urlApi}/user-types/${id}`));
+    return userType;
+  }
+
+  async create(userType: CreateUserType) {
+    await lastValueFrom(this.httpClient.post(`${environment.urlApi}/user-types`, userType));
+    return;
+  }
+
+  async update(id: string, userType: CreateUserType) {
+    await lastValueFrom(this.httpClient.put(`${environment.urlApi}/user-types/${id}`, userType));
+    return;
+  }
+
   async delete(id: string) {
-    await lastValueFrom(this.httpClient.delete(`${environment.urlApi}/user-types/${id}`));
+    await lastValueFrom(this.httpClient.delete(`${environment.urlApi}/user-type/${id}`));
     return;
   }
 }
