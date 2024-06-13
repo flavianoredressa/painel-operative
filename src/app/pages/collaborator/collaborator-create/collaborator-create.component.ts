2ebf159a7aaef38acb30bc596cd0e18f7e66647e
Luana Venancio <luanavenancio73643@gmail.com>
2024-06-13 15:43:27 -0300

    wip: layout cadastro de colaborador

--- src/app/pages/collaborator/collaborator-create/collaborator-create.component.ts
+++ src/app/pages/collaborator/collaborator-create/collaborator-create.component.ts
@@ -1,13 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { getRouterParam } from '@burand/angular';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IsLoadingDirective, getRouterParam } from '@burand/angular';
+import { InputComponent } from '@forms/input/input.component';
+import { errorTailorImports } from '@ngneat/error-tailor';
 @Component({
   selector: 'app-collaborator-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, InputComponent, errorTailorImports, IsLoadingDirective],
   templateUrl: './collaborator-create.component.html'
 })
 export class CollaboratorCreateComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
   idCollaborator = getRouterParam('id');
+  loading = false;
+  submitting = false;
+
+  form = this.formBuilder.group({
+    name: [''],
+    email: [''],
+    phone: [''],
+    journey: [''],
+    admission: [''],
+    birthday: [''],
+    cep: [''],
+    number: [''],
+    address: [''],
+    state: [''],
+    neighborhood: [''],
+    city: [''],
+    active: [true]
+  });
 
   ngOnInit() {
     console.log(this.idCollaborator);
@@ -15,5 +37,9 @@ export class CollaboratorCreateComponent implements OnInit {
     if (this.idCollaborator) {
       console.log('edit');
     }
+  }
+
+  handleSubmit() {
+    console.log(this.form.value);
   }
 }
