f6dde75798219fd136363c12dd73e9a5bf3cceca
Fuzetto-pixel <mayconfuzetto@outlook.com>
2024-06-14 18:05:50 -0300

    feat: cadastro colaborador

--- src/app/core/repositories/collaborator.repository.ts
+++ src/app/core/repositories/collaborator.repository.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AddDocument } from '@burand/angular';
 import { environment } from '@environment';
 import { Collaborator } from '@models/collaborator';
 import { lastValueFrom } from 'rxjs';
+
+type CreateCollaborator = Pick<AddDocument<Collaborator>, 'admission_date' | 'active'>;
 
 @Injectable({
   providedIn: 'root'
@@ -11,18 +14,25 @@ import { lastValueFrom } from 'rxjs';
 export class CollaboratorRepository {
   constructor(private httpClient: HttpClient) {}
 
-  async create(collaborator: Collaborator) {
+  getAll() {
+    return this.httpClient.get<Collaborator[]>(`${environment.urlApi}/collaborator`);
+  }
+
+  async create(collaborator: CreateCollaborator) {
     await lastValueFrom(this.httpClient.post(`${environment.urlApi}/collaborator`, collaborator));
     return;
   }
 
-  async update(id: string, activity: Collaborator) {
-    await lastValueFrom(this.httpClient.put(`${environment.urlApi}/activity/${id}`, activity));
-    return;
+  async getStatusById(id: string) {
+    const collaborator = await lastValueFrom(
+      this.httpClient.get<Collaborator>(`${environment.urlApi}/collaborator/${id}`)
+    );
+    return collaborator;
   }
 
-  getAll() {
-    return this.httpClient.get<Collaborator[]>(`${environment.urlApi}/collaborator`);
+  async update(id: string, collaborator: CreateCollaborator) {
+    await lastValueFrom(this.httpClient.put(`${environment.urlApi}/collaborator/${id}`, collaborator));
+    return;
   }
 
   async delete(id: string) {
