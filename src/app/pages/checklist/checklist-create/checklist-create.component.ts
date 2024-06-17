a5915e4706b91f8d82808136b7c388ef3c7da0de
João Pedro G Furtado <82687549+JoaoPedroFurtado@users.noreply.github.com>
2024-06-17 16:53:29 -0300

    feat(checklist): add status e correções

--- src/app/pages/checklist/checklist-create/checklist-create.component.ts
+++ src/app/pages/checklist/checklist-create/checklist-create.component.ts
@@ -1,20 +1,76 @@
-import { Component, OnInit } from '@angular/core';
-import { getRouterParam } from '@burand/angular';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IsLoadingDirective, getRouterParam } from '@burand/angular';
+import { InputComponent } from '@forms/input/input.component';
+import { errorTailorImports } from '@ngneat/error-tailor';
+import { ChecklistRepository } from '@repositories/checklist.repository';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-checklist-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, InputComponent, errorTailorImports, IsLoadingDirective],
   templateUrl: './checklist-create.component.html'
 })
 export class ChecklistCreateComponent implements OnInit {
+  private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+  private toastrService = inject(ToastrService);
+  private checkListRepository = inject(ChecklistRepository);
+
   idChecklist = getRouterParam('id');
 
-  ngOnInit() {
-    console.log(this.idChecklist);
+  loading = signal(false);
+  submitting = signal(false);
 
-    if (this.idChecklist) {
-      console.log('edit');
+  formGroup = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    active: [true, [Validators.required]]
+  });
+
+  async ngOnInit() {
+    try {
+      if (this.idChecklist) {
+        this.loading.set(true);
+        const checklist = await this.checkListRepository.getStatusById(this.idChecklist);
+        this.formGroup.patchValue(checklist);
+        this.loading.set(false);
+      }
+    } catch (error) {
+      this.loading.set(false);
+      this.toastrService.error('Não foi possível carregar os dados.');
+      console.error(error);
+    }
+  }
+
+  async handleSubmit() {
+    if (this.formGroup.invalid) {
+      this.toastrService.error('Verifique os campos e tenta novamente.');
+      return;
+    }
+
+    this.submitting.set(true);
+
+    try {
+      const { name, active } = this.formGroup.value;
+      const checklist = {
+        active,
+        name
+      };
+
+      if (!this.idChecklist) {
+        await this.checkListRepository.create(checklist);
+      } else {
+        await this.checkListRepository.update(this.idChecklist, checklist);
+      }
+      this.toastrService.success(`CheckList ${!this.idChecklist ? 'cadastrado' : 'atualizado'} com sucesso.`);
+      this.router.navigateByUrl('/checklist');
+    } catch (error) {
+      this.toastrService.error('Não foi possível salvar os dados.');
+      console.error(error);
+    } finally {
+      this.submitting.set(false);
     }
   }
 }
