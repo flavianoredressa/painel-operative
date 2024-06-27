75548f54e5dd058a6442bc6a6ed867ae9096fa1e
João Pedro G Furtado <82687549+JoaoPedroFurtado@users.noreply.github.com>
2024-06-27 14:37:50 -0300

    feat(topbar): ajustes mediante aos erros , quando colocado na develop.

--- src/app/layouts/topbar/topbar.component.ts
+++ src/app/layouts/topbar/topbar.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { ImgFallbackDirective, ShortNamePipe } from '@burand/angular';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
@@ -15,11 +15,11 @@ import { TitleService } from '@services/title.service';
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.comonete.scss'],
-  imports: [NgbDropdownModule, LucideAngularModule, ShortNamePipe, ImgFallbackDirective, NgClass]
+  imports: [LucideAngularModule, ShortNamePipe, ImgFallbackDirective, NgClass, NgbDropdownModule]
 })
-export class TopbarComponent {
+export class TopbarComponent implements OnInit {
   private router = inject(Router);
-  private useSession = inject(SessionContext);
+  private sessionContext = inject(SessionContext);
   public useSideBar = inject(SideBarContext);
   private titleService = inject(TitleService);
 
@@ -27,17 +27,21 @@ export class TopbarComponent {
   isMobile = signal(false);
 
   version = environment.appVersion;
-  currentUser = this.useSession.getLoggedUserFire;
+  currentUser = this.sessionContext.getLoggedUserFire;
 
   constructor() {
     this.isMobile.set(document.documentElement.clientWidth < 768 ? true : false);
+  }
+
+  ngOnInit() {
+    console.log(this.currentUser());
   }
 
   openNotification(): void {}
   changeColor(): void {}
 
   async logout() {
-    await this.useSession.logout();
+    await this.sessionContext.logout();
     await this.router.navigateByUrl('/login');
   }
 }
